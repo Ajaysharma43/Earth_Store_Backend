3ec9a36e04da791a1dc8355b23957781c3c23d6f
ajay sharma <ajaysharma445446@gmail.com>
2025-01-24 19:23:26 +0530

    changes

--- Routes/Authentication.js
+++ Routes/Authentication.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const twillo = require("twilio");
-const jwt = require('jsonwebtoken')
-const Users = require('../Schemma/UserSchemmma')
-const Authenticate = require('../AuthenticateToken/AuthenticateToken')
+const jwt = require("jsonwebtoken");
+const Users = require("../Schemma/UserSchemmma");
+const Authenticate = require("../AuthenticateToken/AuthenticateToken");
 require("dotenv").config();
 
 const app = express();
@@ -22,125 +22,112 @@ router.post("/SentOtp", async (req, res) => {
       from: process.env.TWILLO_NO,
       to: phoneNumber,
     });
-    res.json({ otp }); 
+    res.json({ otp });
   } else {
-    res.json({ message: "phone number not found" }); 
+    res.json({ message: "phone number not found" });
   }
 });
 
+router.post("/SaveData", async (req, res) => {
+  const { Data } = req.body;
 
-router.post('/SaveData' , async(req,res) => {
-    const {Data} = req.body;
-    
-    const existed = await Users.findOne({PhoneNumber : Data.Data.Phone})
-    if(existed)
-    {
-       res.send('existed') 
-    }
-    else
-    {
-      const RefreshToken  = jwt.sign({},process.env.JWT_SECRET_KEY)
-        const set = {UserName : Data.Data.Username , Password : Data.Data.Password ,PhoneNumber: Data.Data.Phone , RefreshToken : RefreshToken}
-        const dataset = new Users(set);
-        const datasave = await dataset.save();
-    }
-    
-})
-
-router.post('/Login' , async(req,res) => {
-  try{ 
-    const {UserName , Password ,PhoneNumber} = req.body;
-    console.log(UserName);
-    
-    const user = await Users.findOne({PhoneNumber : PhoneNumber})
-    if(user)
-    {
-    const payload = { Username : user.UserName , Password : user.Password , phoneNumber : user.Password}
-    const accesstoken  = jwt.sign(payload , process.env.JWT_SECRET_KEY , {expiresIn : 10})
-    const refreshToken = user.RefreshToken;
-
-    res.json({message : "Valid" , token : accesstoken , refreshToken : refreshToken})
-    }
-    else
-    {
-      res.json({message : "not a valid user"})
-    }
-    
+  const existed = await Users.findOne({ PhoneNumber: Data.Data.Phone });
+  if (existed) {
+    res.send("existed");
+  } else {
+    const RefreshToken = jwt.sign({}, process.env.JWT_SECRET_KEY);
+    const set = {
+      UserName: Data.Data.Username,
+      Password: Data.Data.Password,
+      PhoneNumber: Data.Data.Phone,
+      RefreshToken: RefreshToken,
+    };
+    const dataset = new Users(set);
+    const datasave = await dataset.save();
   }
-  catch(error)
-  {
+});
+
+router.post("/Login", async (req, res) => {
+  try {
+    const { UserName, Password, PhoneNumber } = req.body;
+
+    const user = await Users.findOne({ PhoneNumber: PhoneNumber });
+    if (user) {
+      const accesstoken = jwt.sign({}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '2h',
+      });
+      const refreshToken = jwt.sign({}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '7d',
+      });
+
+      res.json({
+        message: "Valid",
+        token: accesstoken,
+        refreshToken: refreshToken,
+      });
+    } else {
+      res.json({ message: "not a valid user" });
+    }
+  } catch (error) {
     console.error("The error Is " + error);
-    
   }
-})
+});
 
-router.post('/VerifyUser' , Authenticate ,  async(req , res) => {
-  try
-  {
-    const {token} = req.body;
-    const verify = jwt.verify(token , process.env.JWT_SECRET_KEY)
-    if(verify)
-    {
-      res.json({message : "token is valid" , verify , user : req.user})
+router.post("/VerifyUser", Authenticate, async (req, res) => {
+  try {
+    const { token } = req.body;
+    const verify = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    if (verify) {
+      res.json({ message: "token is valid", verify, user: req.user });
     }
+  } catch (error) {
+    console.error(error);
   }
-  catch(error)
-    {
-      console.error(error)
-    }
-})
+});
 
-router.post('/VerifyRoute' , Authenticate , async(req , res) => {
-  try 
-  {
+router.post("/VerifyRoute", Authenticate, async (req, res) => {
+  try {
     res.json({ message: "valid", user: req.user });
-  }
-  catch(error)
-  {
+  } catch (error) {
     console.log("the error is " + error);
   }
-})
+});
 
-
-router.post('/RefreshToken', (req, res) => {
+router.post("/RefreshToken", (req, res) => {
   try {
     const { RefreshToken } = req.body;
 
-    if(RefreshToken)
-    {
+    if (RefreshToken) {
       jwt.verify(RefreshToken, process.env.JWT_SECRET_KEY, (err, decoded) => {
         if (err) {
-          if (err.name === 'TokenExpiredError') {
-            console.log('Refresh token expired');
-            res.json({ message: 'expired'}); 
+          if (err.name === "TokenExpiredError") {
+            console.log("Refresh token expired");
+            return res.json({ message: "expired" });
           }
-  
-          console.log('Error verifying refresh token:', err);
-          res.status(401).json({ message: 'Invalid refresh token' });
+
+          console.log("Error verifying refresh token:", err);
+          res.status(401).json({ message: "Invalid refresh token" });
         }
-        console.log('Refresh token verified');
-          const AccessToken = jwt.sign({} , process.env.JWT_SECRET_KEY , {expiresIn : 10})
-          console.log("new token generated");
-          
-          res.json({ message: 'verified' , AccessToken : AccessToken }); 
-        
+        console.log("Refresh token verified");
+        const AccessToken = jwt.sign({}, process.env.JWT_SECRET_KEY, {
+          expiresIn: 10,
+        });
+        console.log("new token generated");
+
+        return res.json({ message: "verified", AccessToken: AccessToken });
       });
+    } else {
+      return res.json({ message: "NotExisted" });
     }
-    else
-    {
-      res.json({message :'NotExisted'})
-    }
-    
   } catch (error) {
-    console.error('Error handling refresh token:', error);
-    return res.status(500).json({ message: 'Internal server error' });
+    console.error("Error handling refresh token:", error);
+    return res.status(500).json({ message: "Internal server error" });
   }
 });
 
-
-router.post('/GenerateRefreshToken' , async(req , res) => {
-  const Token = jwt.sign({} , process.env.JWT_SECRET_KEY , {expiresIn : 10})
-  res.json({Token})
-})
+router.post("/GenerateRefreshToken", async (req, res) => {
+  const Token = jwt.sign({}, process.env.JWT_SECRET_KEY, { expiresIn: 10 });
+  res.json({ Token });
+});
 
 module.exports = router;
