8de8aaec7c05a23f34a366b5ac303866f237ccb7
ajay sharma <ajaysharma445446@gmail.com>
2025-01-30 19:24:58 +0530

    created api to perform operation on the cart data

--- Routes/Cart.js
+++ Routes/Cart.js
@@ -60,7 +60,36 @@ router.delete('/DeleteProduct' , async(req , res) => {
     catch(error)
     {
         console.error(error);
-        res.json({message : error})
+        res.json({Message : error})
+    }
+})
+
+router.delete('/DeleteCart' , async(req , res) => {
+    const {UserID} = req.query;
+    const User = await Users.findOne({_id : UserID});
+    const FindCart = User.CartProducts;
+    if(FindCart)
+    {
+        FindCart.pull();
+        res.json({Message : "Deleted"})
+    }
+    else
+    {
+        res.json({Message : "Not Existed"})
+    }
+})
+
+router.put('/UpdateQunatity' , async(req , res) => {
+    try
+    {
+        const {UserID  , ProductID , Qunatity} = req.body;
+        const User = await Users.findOne({_id : UserID});
+        const Findproduct = User.CartProducts.find((item) => item.ProductID === ProductID)
+        res.json({Message : Findproduct})
+    }
+    catch(error)
+    {
+        console.error(error);
     }
 })
 
